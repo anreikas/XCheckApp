14ed542e9dd84c460a08020702b2d22b2470be05
karway211 <47392703+karway211@users.noreply.github.com>
2020-09-09 22:26:56 +0300

    init: add main app view (#1)
    * init: add main app view
    
    * fix: delete exact login
    
    * fix: add newline at end of file

---
+++ src/js/pages/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import './Header.scss';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header className='app-header'>
+      <NavLink className='main-link' to="/">R&nbsp;&nbsp;&nbsp;C <br/> SCHOOL</NavLink>
+      <h1>Cross-Check RSS</h1>
+      <NavLink to="/login" activeClassName='nav__item-active'>login</NavLink>
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/js/pages/TaskManager/TaskManager.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TaskManager.scss';
+
+const TaskManager = (props) => {
+  return (
+    <div className='task-manager'>
+      Task Manager
+    </div>
+  )
+}
+
+export default TaskManager;
