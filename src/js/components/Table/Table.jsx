3ee3d34375cba4c5c6476b194ee7511899a86dc3
rgovin <rgovin@allvision.by>
2020-09-20 23:42:38 +0300

    feat: table component

--- src/js/components/Table/Table.jsx
+++ src/js/components/Table/Table.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, {
   useState, useEffect, useCallback, useRef,
 } from 'react';
@@ -203,27 +202,7 @@ TableComponent.defaultProps = {
   maxRows: MAX_ROWS,
   onClick: () => {},
 };
+
 export default TableComponent;
-=======
-import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './styles.module.scss';
 
-import {
-  Table, Tag, Space, Input, Button, Popconfirm, Form, Switch,
-} from 'antd';
 
-const Table = ({columns}) => {
-  return (
-    <div>
-
-    </div>
-  );
-};
-
-Table.propTypes = {
-
-};
-
-export default Table;
->>>>>>> feat: loading
--- src/js/utils/index.js
+++ src/js/utils/index.js
@@ -111,13 +111,13 @@ export const UrlConstructor = (url, params, options = {}) => {
     Object.entries(params)
       .map((el) => el.join(equalSign))
       .join(separator)
-    }`;
+  }`;
 };
 
 export const UrlPath = (...args) => args.join('/');
 
 export const FetchReq = async (url, method = 'GET', data) => {
-  console.log( '@FetchReq : url', url );
+  console.log('@FetchReq : url', url);
   const req = {
     method,
     headers: {
@@ -140,11 +140,18 @@ export const FetchReq = async (url, method = 'GET', data) => {
     throw error;
   }
 
-  const {headers} = response;
-  // X-Total-Count
-  console.log( '@FetchReq : ', headers );
-
   const result = await response.json();
 
   return result;
+};
+
+export const TextSorter = (a, b) => {
+  const nameA = a.author.toLowerCase();
+  const nameB = b.author.toLowerCase();
+
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) return 1;
+  return 0; // Никакой сортировки
 };
