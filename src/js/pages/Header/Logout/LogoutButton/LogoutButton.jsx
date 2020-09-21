9517ae34527c53f38018c17913d30d4b58831ca0
Andrey Karchevskiy <karway211@gmail.com>
2020-09-21 19:54:09 +0300

    feat: add authorization with github (#4)

---
+++ src/js/pages/Header/Logout/LogoutButton/LogoutButton.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './LogoutButton.scss';
+import { useAuth0 } from '@auth0/auth0-react';
+
+const LogoutButton = () => {
+  const { logout } = useAuth0();
+  const handleSubmit = () => {
+    localStorage.clear();
+    logout();
+  }
+  return (
+    <button className='logout-bth' onClick={handleSubmit}>
+      Log Out
+    </button>
+  )
+}
+
+export default LogoutButton;
