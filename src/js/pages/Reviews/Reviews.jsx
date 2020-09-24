698bfc66b82633a961297699a670ee7e100637c2
rgovin <rgovin@allvision.by>
2020-09-24 19:59:52 +0300

    feat: review-container

--- src/js/pages/Reviews/Reviews.jsx
+++ src/js/pages/Reviews/Reviews.jsx
@@ -4,14 +4,14 @@ import App from './src/App'
 
 const Reviews = (props) => {
   return (
-    <>
+    <div className="reviews__container">
       <TableComponent
         columns={Columns}
         url={path}
         maxRows={MAX_ROWS}
         onClick={onRowClickHandler}
       />
-    </>
+    </div>
   );
 };
 
