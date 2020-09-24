d9a985b6da8ff28195cb7cfb01f1590ad85c2a92
rgovin <rgovin@allvision.by>
2020-09-24 15:45:10 +0300

    fix: index.js conflict

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,19 @@
-export { default } from './Table';
+import React from 'react';
+import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './pages/app/store';
+import { App } from './pages';
+import { BrowserRouter } from 'react-router-dom';
+
+const Root = () => (
+  <BrowserRouter>
+  <Provider store={store}>
+  <App />
+  </Provider>
+  </BrowserRouter>
+);
+
+ReactDom.render(
+<Root />,
+  document.body.querySelector('#root'),
+);
