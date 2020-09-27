f9967be4057a67953ddfb32d28aa06b8388765db
rgovin <50804620+rgovin@users.noreply.github.com>
2020-09-28 02:17:18 +0300

    Revert "Reviews"

--- src/js/pages/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import thunk from 'redux-thunk';
-import rootReducer from './rootReducer';
-
-export default configureStore({
-  reducer: rootReducer,
-  middleware: [...getDefaultMiddleware(), thunk],
-});
